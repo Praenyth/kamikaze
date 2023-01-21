f47b2ee4156d6bcab730542113f02ac99e0e4e44
Prae <praenyth@gmail.com>
2023-01-20 23:50:23 -0500

    make a firework effect

--- src/main/java/awesome/praenyth/plugins/kamikaze/Kamikaze.java
+++ src/main/java/awesome/praenyth/plugins/kamikaze/Kamikaze.java
@@ -2,19 +2,20 @@ package awesome.praenyth.plugins.kamikaze;
 
 import awesome.praenyth.plugins.kamikaze.events.KillListener;
 import awesome.praenyth.plugins.kamikaze.events.DangerPlacementListener;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Kamikaze extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new KillListener(), this);
+        getServer().getPluginManager().registerEvents(new KillListener(this), this);
         getServer().getPluginManager().registerEvents(new DangerPlacementListener(this), this);
 
     }
 
     @Override
     public void onDisable() {
-
+        HandlerList.unregisterAll(this);
     }
 }
--- src/main/java/awesome/praenyth/plugins/kamikaze/events/KillListener.java
+++ src/main/java/awesome/praenyth/plugins/kamikaze/events/KillListener.java
@@ -2,13 +2,24 @@ package awesome.praenyth.plugins.kamikaze.events;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.plugin.Plugin;
 
 public class KillListener implements Listener {
+
+    private final Plugin plugin;
+
+    public KillListener(Plugin plugin) {
+        this.plugin = plugin;
+    }
 
     @EventHandler
     public void playerOtherSourceDamageListener(EntityDamageEvent e) {
@@ -24,7 +35,6 @@ public class KillListener implements Listener {
                     if (e.getFinalDamage() >= victim.getHealth()) {
 
                         victim.setHealth(20f);
-                        attacker.setHealth(0f);
 
                         e.setCancelled(true);
                         event1.setCancelled(true);
@@ -34,6 +44,20 @@ public class KillListener implements Listener {
                                 Bukkit.getOnlinePlayers()) {
                             player.sendMessage(ChatColor.RED + attacker.getDisplayName() + " has attempted to kill " + victim.getDisplayName() + "!");
                         }
+
+                        Firework firework = attacker.getWorld().spawn(attacker.getLocation(), Firework.class);
+                        FireworkMeta fireworkMeta = firework.getFireworkMeta();
+                        fireworkMeta.setPower(2);
+                        fireworkMeta.addEffect(FireworkEffect.builder().withColor(Color.FUCHSIA, Color.BLACK).withColor(Color.FUCHSIA, Color.BLACK).withColor(Color.FUCHSIA, Color.BLACK).with(FireworkEffect.Type.BALL).trail(true).build());
+                        firework.setFireworkMeta(fireworkMeta);
+
+                        firework.addPassenger(attacker);
+
+                        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+
+                            attacker.setHealth(0f);
+
+                        }, 40);
 
                     }
                 }
